b3ee3fbd01cd0e2f79ad839b030e646fcdf17cfa
ObayIsm <obay.engineer@gmail.com>
2024-04-07 17:52:23 +0300

    Created the generic repository interface and class

---
+++ RepositoryPatternWithUOW.Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPatternWithUOW.Core.Interfaces;
+public interface IBaseRepository<T> where T : class
+{
+    T GetById(int id);
+    IEnumerable<T> GetAll();
+    T Find(Expression<Func<T, bool>> match, string[] includes = null);
+    IEnumerable<T> FindAll(Expression<Func<T, bool>> match, string[] includes = null);
+    IEnumerable<T> FindAll(Expression<Func<T, bool>> match, int take, int skip);
+}
---
+++ RepositoryPatternWithUOW.EF/Repositories/BaseRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryPatternWithUOW.Core.Interfaces;
+using RepositoryPatternWithUOW.EF.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPatternWithUOW.EF.Repositories;
+public class BaseRepository<T> : IBaseRepository<T> where T : class
+{
+    protected AppDbContext _context;
+
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public T GetById(int id) => _context.Set<T>().Find(id);
+
+    public IEnumerable<T> GetAll() => _context.Set<T>().ToList();
+
+    public T Find(Expression<Func<T, bool>> match, string[] includes = null)
+    {
+        IQueryable<T> query = _context.Set<T>();
+
+        if(includes is not null)
+        {
+            foreach(var include in includes)
+            {
+                query = query.Include(include);
+            }
+        }
+
+        return query.FirstOrDefault(match);
+    }
+
+    public IEnumerable<T> FindAll(Expression<Func<T, bool>> match, string[] includes = null)
+    {
+        IQueryable<T> query = _context.Set<T>();
+
+        if (includes is not null)
+        {
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+        }
+
+        return query.Where(match).ToList();
+    }
+
+    public IEnumerable<T> FindAll(Expression<Func<T, bool>> match, int take, int skip)
+    {
+        return _context.Set<T>().Where(match).Skip(skip).Take(take).ToList();
+    }
+}
