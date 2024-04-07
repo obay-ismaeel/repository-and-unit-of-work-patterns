d2f8569ac69a47f509e21eec47c250326eee5966
ObayIsm <obay.engineer@gmail.com>
2024-04-07 21:21:12 +0300

    Created a separate book repository for custom functionality

---
+++ RepositoryPatternWithUOW.EF/Repositories/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using RepositoryPatternWithUOW.Core.Interfaces;
+using RepositoryPatternWithUOW.Core.Models;
+using RepositoryPatternWithUOW.EF.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPatternWithUOW.EF.Repositories;
+public class BookRepository : BaseRepository<Book>, IBookRepository
+{
+    public BookRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public IEnumerable<Book> SpecialMethod()
+    {
+        throw new NotImplementedException();
+    }
+}
