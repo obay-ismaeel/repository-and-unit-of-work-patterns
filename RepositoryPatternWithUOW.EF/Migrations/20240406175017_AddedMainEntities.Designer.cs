8fa3d363ebf83b1b4792b3bbe4bbc18ec5b4feae
ObayIsm <obay.engineer@gmail.com>
2024-04-07 17:50:46 +0300

    Added Migrations

--- RepositoryPatternWithUOW.EF/Migrations/20240406175017_AddedMainEntities.Designer.cs
+++ RepositoryPatternWithUOW.EF/Migrations/20240406175017_AddedMainEntities.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RepositoryPatternWithUOW.EF.Data;
 
@@ -10,9 +11,11 @@ using RepositoryPatternWithUOW.EF.Data;
 namespace RepositoryPatternWithUOW.EF.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240406175017_AddedMainEntities")]
+    partial class AddedMainEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
